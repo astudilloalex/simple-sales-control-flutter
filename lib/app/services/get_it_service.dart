6de943a4b027cb892beb7c3695108221c68b4915
Alex Astudillo <astudilloalex1@gmail.com>
2023-10-22 18:56:05 -0500

    feat: Add company service and repository

--- lib/app/services/get_it_service.dart
+++ lib/app/services/get_it_service.dart
@@ -1,20 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sales_control/src/auth/application/auth_service.dart';
 import 'package:sales_control/src/auth/domain/i_auth_repository.dart';
 import 'package:sales_control/src/auth/infrastructure/firebase_auth_repository.dart';
+import 'package:sales_control/src/company/application/company_service.dart';
+import 'package:sales_control/src/company/domain/i_company_repository.dart';
+import 'package:sales_control/src/company/infrastructure/firebase_company_repository.dart';
 
 GetIt getIt = GetIt.instance;
 
 void setupGetIt() {
   // Declare services to pass to other services.
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   // Declare repositories.
   getIt.registerLazySingleton<IAuthRepository>(
     () => FirebaseAuthRepository(firebaseAuth),
   );
+  getIt.registerLazySingleton<ICompanyRepository>(
+    () => FirebaseCompanyRepository(firestore),
+  );
 
   // Declare services.
-  getIt.registerFactory(() => AuthService(getIt<IAuthRepository>()));
+  getIt.registerFactory<AuthService>(
+      () => AuthService(getIt<IAuthRepository>()));
+  getIt.registerFactory<CompanyService>(
+    () => CompanyService(
+      getIt<ICompanyRepository>(),
+      authRepository: getIt<IAuthRepository>(),
+    ),
+  );
 }
