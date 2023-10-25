cabf0ae57946eb317e0e56dddbeaf7729c59bb12
Alex Astudillo <astudilloalex1@gmail.com>
2023-10-24 22:43:30 -0500

    feat: Add the custom settings

--- lib/ui/pages/setting/setting_page.dart
+++ lib/ui/pages/setting/setting_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sales_control/ui/routes/route_name.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.settings),
+      ),
+      body: GridView.extent(
+        maxCrossAxisExtent: 200.0,
+        children: [
+          InkWell(
+            onTap: () => context.pushNamed(RouteName.editCompany),
+            child: Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(FontAwesomeIcons.building),
+                  Text(AppLocalizations.of(context)!.myCompany),
+                ],
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () => context.pushNamed(RouteName.user),
+            child: Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(FontAwesomeIcons.user),
+                  Text(AppLocalizations.of(context)!.users),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/routes/route_name.dart
+++ lib/ui/routes/route_name.dart
@@ -4,9 +4,11 @@ class RouteName {
   // prevents instantiation and extension.
   const RouteName._();
 
+  static const String editCompany = '/edit-company/:id';
   static const String home = '/home';
   static const String onboarding = '/onboarding';
   static const String setting = '/setting';
   static const String signIn = '/sign-in';
   static const String splash = '/splash';
+  static const String user = '/user';
 }
